70d4f959481d6ce7e352f11862a22512266e8d98
q3world <q3world@sina.com>
2021-09-04 05:41:07 +0800

    Add files via upload

--- tdx.py
+++ tdx.py
@@ -2,9 +2,9 @@ import pywinauto
 import pyautogui
 
 _exe_name = r'D:\new_tdx\TdxW.exe'
-_win_login = '通达信金融终端V7.49'
+_win_login = '通达信金融终端V.*'
 _win_dlg = '通达信信息'
-_win_main = '通达信金融终端V7.49 - [行情报价-沪深Ａ股]'
+_win_main = '通达信金融终端V.* \- '
 _layout = 'q2'
 
 def start(_exe_name):
@@ -20,38 +20,39 @@ def kill(_exe_name):
     except:
         pass
 
-def lonin(window):
-    window.child_window(control_id=0x80ba, class_name='SysTabControl32').select(1)
-    window.child_window(control_id=1, class_name='Button').click()
+def connect(_exe_name):
+    try:
+        app = pywinauto.application.Application().connect(path=_exe_name)
+    except:
+        app = pywinauto.application.Application().start(_exe_name)
+
+    return app
+
+def lonin(app):
+    app.top_window().child_window(control_id=0x80ba, class_name='SysTabControl32').select(1)
+    app.top_window().child_window(control_id=1, class_name='Button').click()
     
-def dlg_close(window):
-    window.close()
+def dlg_close(app):
+    app.top_window().close()
     
-def layout(window):
+def layout(app):
+    app.top_window().set_focus()
     pyautogui.typewrite(_layout, interval=0.01)
     pyautogui.press('enter')
     
 def run():
     app = start(_exe_name)
-    app[_win_login].wait('enabled', timeout=30)
-    window = app.top_window()
-    #print(window.window_text())
-    if window.window_text() == _win_login:
-        lonin(window)
+    result = app.window(title_re=_win_login).wait('enabled', timeout=30)
+    if result:
+        lonin(app)
     
-    try:
-        app[_win_dlg].wait('enabled', timeout=30)
-        window = app.top_window()
-        if window.window_text() == _win_dlg:
-            dlg_close(window)
-            
-    except:
-        pass
+    result = app.window(title_re=_win_dlg).wait('enabled', timeout=30)
+    if result:
+        dlg_close(app)
 
-    app[_win_main].wait('enabled', timeout=30)
-    window = app.top_window()
-    if window.window_text() == _win_main:
-        layout(window)
+    result = app.window(title_re=_win_main).wait('enabled', timeout=30)
+    if result:
+        layout(app)
 
     print('Ok.')
 
